700ded48c1cb797ebbfdfb538783d4ad1563805b
Alex <alexgkt@gmail.com>
2019-12-12 00:14:11 +0800

    Added migration for users tahle

---
+++ database/migrations/2019_12_11_153357_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->string('name', 255);
+            $table->string('username', 30)->unique();
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->text('notes', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
